36b7dd3a69dc2d55132546c6e20f0f4ea341cc09
Kyle Richardson <kyle.richardson1@gmail.com>
2019-09-09 23:22:38 -0500

    bascic setup through step 3, part of 4

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-//import any components needed
+import NumberButton from "./NumberButton"
 
-//Import your array data to from the provided data file
-import {numbers} from "../../../data";
+import {numbers} from "../../../data"
+
 
 const Numbers = () => {
-  // STEP 2 - add the imported data to state
+  const numArray = numbers;
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className="numbers-container">
+      {numArray.map( ele => {
+        return <NumberButton number = {ele} />
+      })}
     </div>
   );
 };
+
+export default Numbers
