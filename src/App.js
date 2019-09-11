fdb5edd8f57ad84198204980fd0417356c45a297
Kyle Richardson <kyle.richardson1@gmail.com>
2019-09-11 01:19:27 -0500

    some basic css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
 import Numbers from  "./components/ButtonComponents/NumberButtons/Numbers"
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators"
 
-import {numbers, operators, specials, operValues} from "./data"
+import {numbers, specials, operValues} from "./data"
 
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -145,14 +145,6 @@ class App extends React.Component {
       })
   }
   }
- 
- 
-
-  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
-  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
-  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
 
   render() {
     return (
